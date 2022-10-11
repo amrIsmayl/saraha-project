609b468d44bcb678f65bea891f2adfaa87c93df1
amr ismayl <105287289+amrIsmayl@users.noreply.github.com>
2022-10-11 17:19:32 +0200

    create validation to users

--- apis/user.apis.js
+++ apis/user.apis.js
@@ -1,10 +1,11 @@
 const { signup, signin } = require('../services/user.service');
+const { userVlidation } = require('../validation/user.validation');
 
 
-const app =require('express').Router();
+const app = require('express').Router();
 
 
-app.post('/signup', signup);
+app.post('/signup/:id', userVlidation, signup);
 app.post('/signin', signin);
 
 module.exports = app
